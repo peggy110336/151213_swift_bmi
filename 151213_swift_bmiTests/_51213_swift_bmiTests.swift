46fcf19a2d1cbebd99ffa6605af42ccde2e93dae
K-Peggy <peggy110336@gmail.com>
2015-12-13 13:17:17 +0800

    Initial Commit

---
+++ 151213_swift_bmiTests/_51213_swift_bmiTests.swift
@@ -0,0 +1,36 @@
+//
+//  _51213_swift_bmiTests.swift
+//  151213_swift_bmiTests
+//
+//  Created by LIPeggy on 2015/12/13.
+//  Copyright © 2015年 LIPeggy. All rights reserved.
+//
+
+import XCTest
+@testable import _51213_swift_bmi
+
+class _51213_swift_bmiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
