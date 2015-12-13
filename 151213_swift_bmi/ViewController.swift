57d13801aa5e2b71d9cf8e47ad6a90d2a2ed8a1c
K-Peggy <peggy110336@gmail.com>
2015-12-13 13:46:55 +0800

    compelete bmi

--- 151213_swift_bmi/ViewController.swift
+++ 151213_swift_bmi/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf_height: UITextField!
+    
+    @IBOutlet weak var tf_weight: UITextField!
+    
+    @IBOutlet weak var label_view: UILabel!
+    
+    @IBAction func btn_calculate(sender: UIButton) {
+        let height: Double? = Double(tf_height.text!)
+        if height == nil {
+            label_view.text = "enter height is wrong!"
+            return
+        }
+        
+        let weight: Double? = Double(tf_weight.text!)
+        if weight == nil {
+            label_view.text = "enter weight is wrong!"
+            return
+        }
+        
+        let bmi: Double = weight! / ((height!/100)*(height!/100))
+        label_view.text = "Your bmi is \(bmi)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
